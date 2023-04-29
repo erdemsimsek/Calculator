35f8a308e5eb14371c60f367b702554b993762f9
Erdem Şimşek <erdemsimsek@users.noreply.github.com>
2023-04-29 17:35:46 +0100

    feedbacks about how to improve the project [1]

--- Calculator/Calculator/Calculator.c
+++ Calculator/Calculator/Calculator.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-https://github.com/MetehanSimsek/Calculator/blob/main/Calculator/Calculator/Calculator.c
+https://github.com/MetehanSimsek/Calculator/blob/main/Calculator/Calculator/Calculator.c //Delete this
 
 int main()
 {
+	// use command line arguments to receive these values
 	int sayi1, sayi2, islem;
 	printf("Lutfen 1.sayiyi giriniz \n");
 	scanf_s("%d", &sayi1);
@@ -12,6 +13,10 @@ int main()
 	printf(" 1. Toplama \n 2. Cikarma \n 3. Carpma \n 4. Bolme\n");
 	scanf_s("%d", &islem);
 
+    // Don't use numbers to represent mathematical operations
+    // Create source file and header file and move addition, subsraction, multiplication and division operations to the new file
+    // math_ops.c and math_ops.h
+	
     switch (islem) {
     case 1:
         printf("Toplama isleminin sonucu : %d", sayi1 + sayi2);
@@ -28,6 +33,6 @@ int main()
     default:
         printf("Lutfen gecerli bir sayi giriniz..");
     }
-
+    // misaligned code, use proper spacing
 	return 0;
 }
