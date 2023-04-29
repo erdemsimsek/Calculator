8ad35dae8f3a82e059dd21bf45c5679a2d8a2e12
MetehanSimsek <71407793+MetehanSimsek@users.noreply.github.com>
2023-04-29 17:09:03 +0300

    Calculator

---
+++ Calculator/Calculator/Calculator.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+int main()
+{
+	int sayi1, sayi2, islem;
+	printf("Lutfen 1.sayiyi giriniz \n");
+	scanf_s("%d", &sayi1);
+	printf("\nLutfen 2.sayiyi giriniz \n");
+	scanf_s("%d", &sayi2);
+	printf("\nLutfen yapilacak islemi seciniz  \n");
+	printf(" 1. Toplama \n 2. Cikarma \n 3. Carpma \n 4. Bolme\n");
+	scanf_s("%d", &islem);
+
+    switch (islem) {
+    case 1:
+        printf("Toplama isleminin sonucu : %d", sayi1 + sayi2);
+        break;
+    case 2:
+        printf("Cýkarma isleminin sonucu : %d", sayi1 - sayi2);
+        break;
+    case 3:
+        printf("Carpma isleminin sonucu : %d", sayi1 * sayi2);
+        break;
+    case 4:
+        printf("Bolme isleminin sonucu : %d", sayi1 * sayi2);
+        break;
+    default:
+        printf("Lutfen gecerli bir sayi giriniz..");
+    }
+
+	return 0;
+}
